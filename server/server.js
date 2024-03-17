4e9624e6b48ee33c70ae9aad93f99702208c2b4a
lifewawj <arevaloaj01@gmail.com>
2024-03-16 18:23:30 -0700

    Fixed Bug by importing the missing packages

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
